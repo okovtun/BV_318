edb346b10e8d2d8a924ca51f9f06ebc73a0e7bce
Clayman <Clayman@DESKTOP-DM8E77U>
2023-12-18 22:46:19 +0300

    while

---
+++ ControlStructures/Calc/main.cpp
@@ -0,0 +1,20 @@
+﻿//Calc
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	double a, b;	//числа, вводимые с клавиатуры
+	char s;			//s - Sign (знак операции)
+	cout << "Введите выражение: ";
+	cin >> a >> s >> b;
+	switch (s)
+	{
+	case '+':	cout << a << " " << s << " " << b << " = " << a + b << endl; break;
+	case '-':	cout << a << " " << s << " " << b << " = " << a - b << endl; break;
+	case '*':	cout << a << " " << s << " " << b << " = " << a * b << endl; break;
+	case '/':	cout << a << " " << s << " " << b << " = " << a / b << endl; break;
+	default:	cout << "Error: No operation" << endl;
+	}
+}
