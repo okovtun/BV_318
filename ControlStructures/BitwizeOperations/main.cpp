5026ede883324e82c981ba033bd1f08a6f55dfa5
Clayman <Clayman@DESKTOP-DM8E77U>
2024-01-29 21:23:14 +0300

    Pascal triangle

--- ControlStructures/BitwizeOperations/main.cpp
+++ ControlStructures/BitwizeOperations/main.cpp
@@ -4,9 +4,19 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-	unsigned short number = 5;
+	/*unsigned short number = 5;
 	cout << (unsigned short)number << endl;
 	cout << (unsigned short)~number << endl;
 	cout << (5 ^ 3) << endl;
-	cout << (5 ^ 5) << endl;
+	cout << (5 ^ 5) << endl;*/
+	int a = 2;
+	int b = 3;
+	cout << a << "\t" << b << endl;
+	/*a ^= b;
+	b ^= a;
+	a ^= b;*/
+
+	b ^= a ^= b ^= a;
+	
+	cout << a << "\t" << b << endl;
 }
---
+++ ControlStructures/PascalTriangle/main.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+using namespace std;
+
+#define WIDTH	8
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int h;
+	cout << "Введите высоту треугольника: "; cin >> h;
+	int nf = 1;
+	for (int i = 0; i < h; i++)
+	{
+		cout.width(WIDTH / 2);
+		cout << "";
+	}
+	cout << left;	//задаем выравнивание по левому краю
+	cout << 1 << endl;
+	for (int n = 1; n <= h; n++)
+	{
+		nf *= n;
+		for (int i = 0; i < h - n; i++)
+		{
+			cout.width(WIDTH/2);
+			cout << "";
+		}
+		int mf = 1;
+		cout.width(WIDTH);
+		cout << 1;
+		for (int m = 1; m <= n; m++)
+		{
+			mf *= m;
+			int mnf = 1;
+			for (int mn = 1; mn <= n - m; mn++)
+			{
+				mnf *= mn;
+			}
+			cout.width(WIDTH);	//задаем ширину вывода, а именно, сколько знакопозиций будет занимать выводимое значение.
+			//cout.width() воздействует только на одно выводимое значение, для следующего выводимого значения, опять нужно задавать ширину.
+			cout << nf / mf / mnf;
+		}
+		cout << endl;
+	}
+}
