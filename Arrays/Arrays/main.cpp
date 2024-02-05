8d5203caaf00e28ba297014b303240ced150ad3d
Clayman <Clayman@DESKTOP-DM8E77U>
2024-02-05 21:40:43 +0300

    Introduction to Arrays

--- Arrays/Arrays/main.cpp
+++ Arrays/Arrays/main.cpp
@@ -5,5 +5,23 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
+	cout << "Hello Arrays" << endl;
 
+	const int SIZE = 5;
+	int arr[SIZE] = { 3, 5, 8 };
+	cout << int() << endl;
+	//arr[2] = 2048;			//обращение к элементу массива на запись
+	//cout << arr[2] << endl;	//обращение к элементу массива на чтение
+	//SIZE - 1
+	//	 n - 1
+
+	//Ввод элементов массива с клавиатуры:
+
+
+	//Вывод массива на экран:
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
 }
