392e0ef8dc97a6c4f7a24dd0966d0dd9660401f9
Clayman <Clayman@DESKTOP-DM8E77U>
2024-02-20 13:56:33 +0300

    Shift DONE

---
+++ Arrays/Binary/main.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int decimal;	//десятичное число
+	cout << "Введите десятичное число: "; cin >> decimal;
+	const int n = 32;
+	bool bin[n] = {};	//Этот массив будет хранить разряды двоичного числа
+#ifdef VAR_1
+	int i = 0;
+	while (decimal > 0)
+	{
+		bin[i] = decimal % 2;	//получаем младший разряд двоичного числа
+		i++;	//переходим к следующему разряду
+		decimal /= 2;		//убираем младший двоичный разряд из числа
+	}
+#endif // VAR_1
+
+	int i = 0;
+	//for(start;stop;step);
+	for (; decimal; bin[i++] = decimal % 2, decimal /= 2);
+
+	for (--i; i >= 0; i--)
+	{
+		cout << bin[i];
+	}
+	cout << endl;
+}
---
+++ Arrays/Shift/main.cpp
@@ -0,0 +1,39 @@
+﻿#pragma warning(disable:4326)
+#include<Windows.h>
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	int number_of_shifts;
+	cout << "На сколько элементов сдвинуть массив: "; cin >> number_of_shifts;
+
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 0; i < n - 1; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = buffer;
+		system("CLS");	//Функция system(...) вызывает любую команду Windows
+		//Команда 'CLS' - Clear Screen, очищает окно консоли.
+		system("calc");
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << "\t";
+		}
+		cout << endl;
+		Sleep(1000);	//Функция Sleep(...) приостанавливает выполнение программы на заданный промежуток времени
+		//Промежуток времени задается в миллисекундах.
+	}
+}
