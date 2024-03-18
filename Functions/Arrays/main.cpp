def9131c80f1d79b0f0c0be6b995235aa86d9a3f
Clayman <Clayman@DESKTOP-DM8E77U>
2024-03-18 21:45:18 +0300

    Arrays functions in progress...

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -4,19 +4,42 @@ using namespace std;
 
 #define tab "\t"
 
+void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
+
+void Print(const int arr[], const int n);
+void Print(const double arr[], const int n);
+
+int  Sum(const int arr[], const int n);
+double Avg(const int arr[], const int n);
+
 void main()
 {
 	setlocale(LC_ALL, "");
 	const int n = 5;
 	int arr[n];
 
-	int minRand, maxRand;
+	/*int minRand, maxRand;
 	do
 	{
 		cout << "Введите минимально возможное случайное число: "; cin >> minRand;
 		cout << "Введите максимально возможное случайное число: "; cin >> maxRand;
 		if (minRand == maxRand)cout << "Пределы диапазона не должны совпадать" << endl;
-	} while (minRand == maxRand);
+	} while (minRand == maxRand);*/
+
+	FillRand(arr, n);
+	Print(arr, n);
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(arr, n) << endl;
+
+	const int m = 8;
+	double brr[m];
+	FillRand(brr, m);
+	Print(brr, m);
+}
+
+void FillRand(int arr[], const int n, int minRand, int maxRand)
+{
 	if (maxRand < minRand)
 	{
 		int buffer = minRand;
@@ -31,11 +54,57 @@ void main()
 		//Функция rand() генерирует псевдослучайное число в диапазоне от 0 до 32 767 (RAND_MAX)
 		//Это псевдослучайное число можно вывести на экран, сохранить в переменную, или элемент массива.
 	}
+}
+void FillRand(double arr[], const int n, int minRand, int maxRand)
+{
+	if (maxRand < minRand)
+	{
+		int buffer = minRand;
+		minRand = maxRand;
+		maxRand = buffer;
+	}
+	//Заполнение массива случайными числами:
+	minRand *= 100;
+	maxRand *= 100;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = minRand + rand() % (maxRand - minRand);
+		arr[i] /= 100;
+		//arr[i] = 50 + rand() % 50;
+		//Функция rand() генерирует псевдослучайное число в диапазоне от 0 до 32 767 (RAND_MAX)
+		//Это псевдослучайное число можно вывести на экран, сохранить в переменную, или элемент массива.
+	}
+}
 
+void Print(const int arr[], const int n)
+{
 	//Вывод массива на экран:
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << tab;
 	}
 	cout << endl;
+}
+void Print(const double arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+
+int  Sum(const int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+double Avg(const int arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
 }
