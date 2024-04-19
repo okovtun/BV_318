22550fa51ab745a6e9d0fc10f759764587c4afd2
Clayman <Clayman@DESKTOP-DM8E77U>
2024-04-19 20:30:02 +0300

    All DONE

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -6,14 +6,15 @@ using namespace std;
 
 void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
 void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(char arr[], const int n, int minRand = 0, int maxRand = 100);
+const char* Sum(char arr[], const int n);
+const char* Avg(char arr[], const int n);
 //template - создает шаблон
 //typename - создает шаблонный тип данных
 //T - имя шаблонного типа
-template<typename T>
-void Print(const T arr[], const int n);
-template<typename T>
-T Sum(const T arr[], const int n);
-double Avg(const int arr[], const int n);
+template<typename T>void Print(const T arr[], const int n);
+template<typename T>T Sum(const T arr[], const int n);
+template<typename T>double Avg(const T arr[], const int n);
 
 void main()
 {
@@ -31,6 +32,14 @@ void main()
 	FillRand(d_arr, D_SIZE);
 	Print(d_arr, D_SIZE);
 	cout << "Сумма элементов массива: " << Sum(d_arr, D_SIZE) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(d_arr, D_SIZE) << endl;
+
+	const int C_SIZE = 11;
+	char c_arr[C_SIZE];
+	FillRand(c_arr, C_SIZE);
+	Print(c_arr, C_SIZE);
+	cout << "Сумма элементов массива: " << Sum(c_arr, C_SIZE) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(c_arr, C_SIZE) << endl;
 }
 
 void FillRand(int arr[], const int n, int minRand, int maxRand)
@@ -70,8 +79,33 @@ void FillRand(double arr[], const int n, int minRand, int maxRand)
 		//Это псевдослучайное число можно вывести на экран, сохранить в переменную, или элемент массива.
 	}
 }
-template<typename T>
-void Print(const T arr[], const int n)
+void FillRand(char arr[], const int n, int minRand, int maxRand)
+{
+	if (maxRand < minRand)
+	{
+		int buffer = minRand;
+		minRand = maxRand;
+		maxRand = buffer;
+	}
+	//Заполнение массива случайными числами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand();
+		//arr[i] = 50 + rand() % 50;
+		//Функция rand() генерирует псевдослучайное число в диапазоне от 0 до 32 767 (RAND_MAX)
+		//Это псевдослучайное число можно вывести на экран, сохранить в переменную, или элемент массива.
+	}
+}
+const char* Sum(char arr[], const int n)
+{
+	return "Символы не суммируются";
+}
+const char* Avg(char arr[], const int n)
+{
+	return "Для типа 'char' среднее-арифметическое не считается";
+}
+
+template<typename T>void Print(const T arr[], const int n)
 {
 	//Вывод массива на экран:
 	for (int i = 0; i < n; i++)
@@ -80,8 +114,7 @@ void Print(const T arr[], const int n)
 	}
 	cout << endl;
 }
-template<typename T>
-T Sum(const T arr[], const int n)
+template<typename T>T Sum(const T arr[], const int n)
 {
 	T sum = 0;
 	for (int i = 0; i < n; i++)
@@ -90,7 +123,7 @@ T Sum(const T arr[], const int n)
 	}
 	return sum;
 }
-double Avg(const int arr[], const int n)
+template<typename T>double Avg(const T arr[], const int n)
 {
 	return (double)Sum(arr, n) / n;
 }
