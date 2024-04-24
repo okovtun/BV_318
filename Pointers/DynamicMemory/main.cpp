b0f1b6f9e06a15191277f37bd592b9065ff66f99
Clayman <Clayman@DESKTOP-DM8E77U>
2024-04-24 22:05:02 +0300

    Introduction to DynamicMemory

---
+++ Pointers/DynamicMemory/main.cpp
@@ -0,0 +1,28 @@
+﻿//DynamicMemory
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер массива: "; cin >> n;
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		//Обращение к элементам массива через арифметику указателей 
+		//и оператор разыменования:
+		*(arr + i) = rand() % 100;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		//Обращение к элементам массива через оператор индексирования - []:
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	delete[] arr;
+}
