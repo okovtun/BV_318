40baa4c62e658c23eb9e5c82b4f8349e7fe6c0b6
Clayman <Clayman@DESKTOP-DM8E77U>
2024-04-26 21:59:22 +0300

    delete breakpoint

--- Pointers/DynamicMemory/main.cpp
+++ Pointers/DynamicMemory/main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 #define tab "\t"
 
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -11,18 +14,33 @@ void main()
 	cout << "Введите размер массива: "; cin >> n;
 	int* arr = new int[n];
 
+	FillRand(arr, n);
+	Print(arr, n);
+
+	int value;
+	cout << "Введите добавляемое значение: "; cin >> value;
+	arr[n] = value;
+	n++;
+	Print(arr, n);
+
+	delete[] arr;
+}
+
+void FillRand(int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		//Обращение к элементам массива через арифметику указателей 
 		//и оператор разыменования:
 		*(arr + i) = rand() % 100;
 	}
+}
+void Print(int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		//Обращение к элементам массива через оператор индексирования - []:
 		cout << arr[i] << tab;
 	}
 	cout << endl;
-
-	delete[] arr;
 }
