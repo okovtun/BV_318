9b94e8ef386161ab9667f8c2badc7266d5ba2889
Clayman <Clayman@DESKTOP-DM8E77U>
2024-04-30 00:38:25 +0300

    pop_back DONE

--- Pointers/DynamicMemory/main.cpp
+++ Pointers/DynamicMemory/main.cpp
@@ -1,15 +1,31 @@
 ﻿//DynamicMemory
 #include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 #define tab "\t"
 
 void FillRand(int arr[], const int n);
+void FillRand(int** arr, const int rows, const int cols);
 void Print(int arr[], const int n);
+void Print(int** arr, const int rows, const int cols);
+
+int* push_back(int arr[], int& n, int value);
+int* push_front(int arr[], int& n, int value);
+int* pop_back(int arr[], int& n);
+//Stack - это модель памяти, из которой последний записанный элемент считывается первым.
+//push
+//pop
+
+//#define DYNAMIC_MEMORY_1
+#define DYNAMIC_MEMORY_2
 
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef DYNAMIC_MEMORY_1
 	int n;
 	cout << "Введите размер массива: "; cin >> n;
 	int* arr = new int[n];
@@ -19,11 +35,46 @@ void main()
 
 	int value;
 	cout << "Введите добавляемое значение: "; cin >> value;
-	arr[n] = value;
-	n++;
+	cout << &n << endl;
+	arr = push_back(arr, n, value);
+	Print(arr, n);
+	cout << "Введите добавляемое значение: "; cin >> value;
+	arr = push_front(arr, n, value);
 	Print(arr, n);
 
+	arr = pop_back(arr, n);
+	Print(arr, n);
+
+	//delete[] buffer;
 	delete[] arr;
+#endif // DYNAMIC_MEMORY_1
+
+#ifdef DYNAMIC_MEMORY_2
+	int rows;
+	int cols;
+	cout << "Введите количество строк: "; cin >> rows;
+	cout << "Введите количество элементов строки: "; cin >> cols;
+
+	//1) Создаем массив указателей:
+	int** arr = new int*[rows];
+	//2) Выделяем память под строки:
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols] {};
+	}
+
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+
+	//1) Сначала удаляем строки:
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+	//2) Удаляем массив указателей:
+	delete[] arr;
+#endif // DYNAMIC_MEMORY_2
+
 }
 
 void FillRand(int arr[], const int n)
@@ -35,6 +86,19 @@ void FillRand(int arr[], const int n)
 		*(arr + i) = rand() % 100;
 	}
 }
+void FillRand(int** arr, const int rows, const int cols)
+
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+
+}
+
 void Print(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
@@ -43,4 +107,60 @@ void Print(int arr[], const int n)
 		cout << arr[i] << tab;
 	}
 	cout << endl;
+}
+void Print(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+int* push_back(int arr[], int& n, int value)
+{
+	cout << &n << endl;
+	//////////////////////////////////////////////
+		//1) Создаем буферный массив нужного размера:
+	int* buffer = new int[n + 1];
+	//2) Копируем значения из исходного массива в буферный:
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	//3) Удаляем исходный массив:
+	delete[] arr;
+	//4) Подменяем адрес исходного массива адресом нового массива:
+	arr = buffer;
+	//5) Только после этого в конеце массива появляется свободное место,
+	//	 куда можно добавить значение:
+	arr[n] = value;
+	n++;
+	//////////////////////////////////////////////
+	//Print(arr, n);
+	return arr;
+}
+int* push_front(int arr[], int& n, int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i + 1] = arr[i];
+	}
+	delete[] arr;
+	buffer[0] = value;
+	n++;
+	return buffer;
+}
+
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
 }
