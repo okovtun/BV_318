54c35061384f87f5a31d432d5a046a12f5c87b0e
Clayman <Clayman@DESKTOP-DM8E77U>
2024-05-11 09:57:35 +0300

    push_row_back DONE

--- Pointers/DynamicMemory/main.cpp
+++ Pointers/DynamicMemory/main.cpp
@@ -7,7 +7,10 @@ using std::endl;
 
 #define tab "\t"
 
-void FillRand(int arr[], const int n);
+int** Allocate(const int rows, const int cols);
+void  Clear(int** arr, const int rows);
+
+void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
 void FillRand(int** arr, const int rows, const int cols);
 void Print(int arr[], const int n);
 void Print(int** arr, const int rows, const int cols);
@@ -19,8 +22,11 @@ int* pop_back(int arr[], int& n);
 //push
 //pop
 
+int** push_row_back(int** arr, int& rows, const int cols);
+
 //#define DYNAMIC_MEMORY_1
-#define DYNAMIC_MEMORY_2
+//#define DYNAMIC_MEMORY_2
+#define PREFORMANCE_CHECK
 
 void main()
 {
@@ -55,17 +61,52 @@ void main()
 	cout << "Введите количество строк: "; cin >> rows;
 	cout << "Введите количество элементов строки: "; cin >> cols;
 
+	int** arr = Allocate(rows, cols);
+
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+
+	arr = push_row_back(arr, rows, cols);
+	FillRand(arr[rows - 1], cols, 100, 1000);
+	Print(arr, rows, cols);
+
+	Clear(arr, rows);
+#endif // DYNAMIC_MEMORY_2
+
+#ifdef PREFORMANCE_CHECK
+	int rows;
+	int cols;
+	cout << "Введите количество строк: "; cin >> rows;
+	cout << "Введите количество элементов строки: "; cin >> cols;
+
+	int** arr = Allocate(rows, cols);
+	cout << "Memory allocated" << endl;
+	system("PAUSE");
+
+	arr = push_row_back(arr, rows, cols);
+	cout << "Row added" << endl;
+	system("PAUSE");
+
+	Clear(arr, rows);
+	cout << "Memory clean" << endl;
+#endif // PREFORMANCE_CHECK
+
+}
+
+int** Allocate(const int rows, const int cols)
+{
 	//1) Создаем массив указателей:
 	int** arr = new int*[rows];
+
 	//2) Выделяем память под строки:
 	for (int i = 0; i < rows; i++)
 	{
 		arr[i] = new int[cols] {};
 	}
-
-	FillRand(arr, rows, cols);
-	Print(arr, rows, cols);
-
+	return arr;
+}
+void  Clear(int** arr, const int rows)
+{
 	//1) Сначала удаляем строки:
 	for (int i = 0; i < rows; i++)
 	{
@@ -73,17 +114,15 @@ void main()
 	}
 	//2) Удаляем массив указателей:
 	delete[] arr;
-#endif // DYNAMIC_MEMORY_2
-
 }
 
-void FillRand(int arr[], const int n)
+void FillRand(int arr[], const int n, int minRand, int maxRand)
 {
 	for (int i = 0; i < n; i++)
 	{
 		//Обращение к элементам массива через арифметику указателей 
 		//и оператор разыменования:
-		*(arr + i) = rand() % 100;
+		*(arr + i) = rand() % (maxRand - minRand) + minRand;
 	}
 }
 void FillRand(int** arr, const int rows, const int cols)
@@ -162,5 +201,25 @@ int* pop_back(int arr[], int& n)
 	int* buffer = new int[--n];
 	for (int i = 0; i < n; i++)buffer[i] = arr[i];
 	delete[] arr;
+	return buffer;
+}
+
+int** push_row_back(int** arr, int& rows, const int cols)
+{
+	//1) Создаем буферный массив указателей нужного размера:
+	int** buffer = new int*[rows + 1];
+
+	//2) Копируем строки из исходного массива в массив указателей:
+	for (int i = 0; i < rows; i++)buffer[i] = arr[i];
+
+	//3) Удаляем исходный массив указателей:
+	delete[] arr;
+
+	//4) Создаем строку и добавляем ее в массив:
+	buffer[rows] = new int[cols] {};
+
+	//5) После добавления строки в массив, количество его строк увеличивается:
+	rows++;
+
 	return buffer;
 }
